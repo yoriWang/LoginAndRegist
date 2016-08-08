f12a413bd474af87b890a7f0468668804f47305c
AlanZeng <alan.zeng@ficent.com>
2016-08-08 11:27:37 +0800

    add a project demo

---
+++ LoginAndRegistDemo/LoginAndRegistDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginAndRegistDemo
+//
+//  Created by FicentDev on 8/6/16.
+//  Copyright © 2016 yori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginAndRegistDemo/LoginAndRegistDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginAndRegistDemo
+//
+//  Created by FicentDev on 8/6/16.
+//  Copyright © 2016 yori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
